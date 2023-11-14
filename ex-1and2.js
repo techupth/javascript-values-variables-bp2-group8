ae0172da7a50b8a42b1fb8e7c116dc796f4cc97d
fonytas <fonyfonyta@gmail.com>
2023-11-14 12:22:25 +0700

    Feat-Values-Variables Assignment:Completed Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = 'Karter';
+let lastName = 'Anderson';
+let Address = '54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bangkruai';
+const restaurantName = "Chef's Table by Chef James";
+const totalCost = 15000;
+const totalRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = '(970) 878-53292400 Country Rd #4Meeker, Colorado(CO), 81641';
+totalCost = 5000;
+totalRestaurant = 2500;
+
