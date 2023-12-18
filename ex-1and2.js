dbf17bd68cbf077441f11397239f175a3c4e43c7
Krittin <rubruengyod@gmail.com>
2023-12-18 16:53:38 +0700

    feat:JS assingment completed

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,15 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
-let firstName = 'Karter';
-let lastName = 'Anderson';
-let Address = '54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bangkruai';
-const restaurantName = "Chef's Table by Chef James";
-const totalCost = 15000;
-const totalRestaurant = 1500;
+let firstName = "Karter"
+let lastName = "Anderson"
+let userAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Krauy Bang Kruai"
+const restaurantName = "Chef's table by Chef James"
+const totalPrice = 15000
+const totalRestaurent = 1500
 
 //Exercise #2: Variable Reassignment
 //Start coding here
-Address = '(970) 878-53292400 Country Rd #4Meeker, Colorado(CO), 81641';
-totalCost = 5000;
-totalRestaurant = 2500;
-
+userAddress = "(970) 878-53292400 Country Rd#4Meeker, Colorado(CO), 81641"
+totalPrice = 5000
+totalRestaurent = 2500
